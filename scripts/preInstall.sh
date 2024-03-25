38b81d53b558deff0265df69bd13ce74aeb85365
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-03-25 14:25:14 +0200

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,3 +1,10 @@
 #set env vars
 set -o allexport; source .env; set +o allexport;
 
+mkdir -p ./volumes/public
+mkdir -p ./volumes/private
+mkdir -p ./volumes/config
+chown -R 1000:1000 ./volumes/public
+chown -R 1000:1000 ./volumes/private
+chown -R 1000:1000 ./volumes/config
+chmod -R 777 ./volumes
